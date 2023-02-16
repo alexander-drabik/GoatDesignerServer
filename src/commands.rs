a16e4db1e483bd6b9282452ca939e188fd694170
alexander-drabik <xelada91@o2.pl>
2023-02-16 18:26:28 +0100

    Respond to version check

---
+++ src/commands.rs
@@ -0,0 +1,3 @@
+pub enum Commands {
+    VersionCheck = 0,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+mod commands;
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+use std::env;
+use crate::commands::Commands;
+
+#[tokio::main]
+async fn main() {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:42069".to_string());
+
+    let listener = TcpListener::bind(&addr).await.unwrap();
+
+    loop {
+        let (mut socket, _) = listener.accept().await.unwrap();
+
+        tokio::spawn( async move {
+            let mut buffer = vec![0; 1024];
+
+            loop {
+                let n = socket.read(&mut buffer).await.unwrap();
+                if n == 0 {
+                    return
+                }
+
+                if buffer[0] == Commands::VersionCheck as u8 {
+                    let version = "1.0".as_bytes();
+
+                    socket.write_all(&version).await.expect("failed to write data")
+                }
+            }
+        });
+    }
 }
