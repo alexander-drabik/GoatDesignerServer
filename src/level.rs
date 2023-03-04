18b7b114f339b9d441cc093aae56b521bb6168ed
alexander-drabik <xelada91@o2.pl>
2023-03-04 19:38:26 +0100

    Add things idk

--- src/level.rs
+++ src/level.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use tokio::fs;
 use serde::{Deserialize, Serialize};
+use tokio::fs::OpenOptions;
 
 #[derive(Serialize, Deserialize)]
 pub struct Level {
@@ -14,5 +15,33 @@ impl Level {
         let data = fs::read(path).await.expect("Unable to read file");
         let level: Level = serde_json::from_slice(&*data).unwrap();
         level
+    }
+
+    pub async fn save_level(&self) -> u8 {
+        let file = OpenOptions::new().read(true).open(format!("./levels/{}", self.name)).await;
+        match file {
+            Ok(_) => 0,
+            Err(_) => {
+                let json = serde_json::to_vec(self).expect("TAK");
+                fs::write( format!("./levels/{}", self.name), json).await.unwrap();
+                1
+            }
+        }
+    }
+
+    pub async fn save_level_data(path: String, path2: String, data: &Vec<u8>) {
+        let file1 = OpenOptions::new().read(true).open(&path2).await;
+        match file1 {
+            Ok(_) => {},
+            Err(_) => { return}
+        }
+
+        let file = OpenOptions::new().read(true).open(&path).await;
+        match file {
+            Ok(_) => {}
+            Err(_) => {
+                fs::write(path, data).await.unwrap();
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,16 @@ use tokio::net::TcpListener;
 
 use std::env;
 use std::fs::{read, read_dir};
+use std::net::IpAddr;
 use std::os::linux::fs;
+use std::os::unix::net::SocketAddr;
 use std::path::Path;
+use std::sync::Arc;
 use serde::de::Unexpected::Str;
+use serde_json::Value::Array;
+use tokio::sync::Mutex;
+use tokio::task;
+use tokio::time::Instant;
 use crate::level::Level;
 
 #[tokio::main]
@@ -18,11 +25,24 @@ async fn main() {
 
     let listener = TcpListener::bind(&addr).await.unwrap();
 
+    let addr_list: Arc<Mutex<Vec<IpAddr>>> = Arc::new(Mutex::new(Vec::new()));
+    let mut cleared_at = Arc::new(Instant::now());
+
+    let addr_clone = addr_list.clone();
+    let clear = task::spawn(async move {
+        loop {
+            if cleared_at.elapsed().as_secs() > 5 * 60 {
+                cleared_at = Arc::new(Instant::now());
+                addr_clone.lock().await.clear();
+            }
+        }
+    });
     loop {
+        let current_addr_list = addr_list.clone();
         let (mut socket, _) = listener.accept().await.unwrap();
 
         tokio::spawn( async move {
-            let mut buffer = vec![0; 1024];
+            let mut buffer = vec![0; (1024*1024)*5];
 
             loop {
                 let n = socket.read(&mut buffer).await.unwrap();
@@ -44,12 +64,13 @@ async fn main() {
                         let paths = read_dir("./levels/").unwrap();
                         let mut level_array: Vec<Level> = vec![];
                         for (index, path) in paths.enumerate() {
-                            if index < ((page - 1) * 5) as usize {
+                            if index <= ((page - 1) * 4) as usize {
                                 continue
                             }
                             let level = Level::load_level(path.unwrap().path().as_path()).await;
                             level_array.push(level);
-                            if index > ((page - 1) * 5 + 5) as usize {
+                            println!("{}", index);
+                            if index >= ((page - 1) * 4 + 4) as usize {
                                 break;
                             }
                         }
@@ -70,6 +91,51 @@ async fn main() {
                         println!("{}", answer.len());
 
                         socket.write_all(&answer).await.expect("failed to respond")
+                    }
+                    3 => {
+                        let data = buffer[1..n].to_vec();
+
+                        let level_option: Option<Level> = serde_json::from_slice(&data)
+                            .unwrap_or_else(|_| return None);
+
+                        match level_option {
+                            None => {
+                                let _ = socket.write(&[0 as u8; 1]);
+                            }
+                            Some(mut level) => {
+                                let mut is_present = false;
+                                let mut ips = current_addr_list.lock().await;
+                                for ip in ips.iter() {
+                                    if socket.peer_addr().unwrap().ip() == *ip {
+                                        is_present = true;
+                                    }
+                                }
+                                let mut buffer = [0_u8; 1];
+                                if is_present {
+                                    buffer[0] = 2;
+                                    let _ = socket.write_all(&buffer).await;
+                                } else {
+                                    level.author = socket.peer_addr().expect("").ip().to_string();
+                                    let response = level.save_level().await;
+                                    buffer[0] = response;
+                                    if response == 0  {
+                                        let _ = socket.write_all(&buffer).await;
+                                    } else {
+                                        ips.push(socket.peer_addr().unwrap().ip());
+                                        let _ = socket.write_all(&buffer).await;
+                                    }
+                                }
+                                drop(ips)
+                            }
+                        }
+                    }
+                    4 => {
+                        let name = String::from_utf8(
+                            buffer[1..10].to_vec().into_iter().filter(|x| x != &0_u8).collect::<Vec<u8>>()
+                        ).expect("name");
+                        let data = buffer[10..n].to_vec();
+
+                        Level::save_level_data(format!("./level_data/{}", name), format!("./levels/{}", name),&data).await;
                     }
                     _ => {}
                 }
